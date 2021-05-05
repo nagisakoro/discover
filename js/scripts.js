6dbc3a041d1a3bfb16e0c16ce7567b38dc195b4c
Nagisa_Koro <ivanchenko680@gmail.com>
2021-05-05 23:46:20 +0800

    добавил сервис и блюр

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,23 @@
-$(function() {
-    $('.mini-menu').click(function(){
-        if($('nav ul').css('display') == 'none'){
-        $('nav ul').slideDown();
-    } else {
-        $('nav ul').slideUp();
-    }
-});
-$('.service-slider').slick({
-    arrows: false,
-    dots: true,
-})
+$(function () {
+	$('.mini-menu').click(function () {
+		if ($('nav ul').css('display') == 'none') {
+			$('nav ul').slideDown();
+		} else {
+			$('nav ul').slideUp();
+		}
+	});
+
+	$('.service-slider').slick({
+		arrows: false,
+		dots: true,
+		responsive: [{
+			breakpoint: 600,
+			settings: {
+				dots: false,
+				centerMode: true,
+				centerPadding: '50px',
+			} 
+		}]
+	});
+	AOS.init();
 });
